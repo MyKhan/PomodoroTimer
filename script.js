42f8b6d390ac97f3920551c1d4280a4b3394737c
Meher Yar Khan <meheryarkhan90@hotmail.com>
2018-04-08 23:16:34 +0300

    Add files via upload
    Add and Subtract Time Buttons to be added.

--- script.js
+++ script.js
@@ -1,68 +1,91 @@
-var timerMinutes = document.getElementById('timer-minutes').innerHTML;
-var timerSeconds = document.getElementById('timer-seconds').innerHTML;
-var canvas = document.getElementById('canvas');
-var pomodoroDiv = document.getElementById('div-pomodoro');
-var rotateButton = document.getElementById('button_rotate');
+var timerMinutes = document.getElementById("timer-minutes").innerHTML;
+var timerSeconds = document.getElementById("timer-seconds").innerHTML;
+
+var canvas = document.getElementById("canvas");
+var pomodoroDiv = document.getElementById("div-pomodoro");
+var rotateButton = document.getElementById("button_rotate");
 var startButton = document.getElementById("start");
 var stopButton = document.getElementById("stop");
 var resetButton = document.getElementById("reset");
-var canvasDegrees = 0;
-var pomodoroDegrees = 0;
+
 var timer;
+var stateToEstablish = false;
+var timerMiliSeconds = timerMinutes * 60000 + timerSeconds * 1000;
+
 stopButton.disabled = true;
 resetButton.disabled = true;
 
-function countdownTimerBegin(){
-  beginRotations();
-  timer = setInterval(function(){
-    if (timerMinutes > 0 && timerSeconds == 0){
-      timerMinutes -= 1;
-      timerSeconds = 59;
-      beginRotations();
-      
-    }
-    else if (timerMinutes == 0 && timerSeconds == 0) {
-      alert("Time's up!");
-    }
-    else {
-      timerSeconds -= 1;
-      beginRotations();
-    }
-    document.getElementById('timer-minutes').innerHTML = timerMinutes;
-    document.getElementById('timer-seconds').innerHTML = timerSeconds;
-    // canvas.style.transform = "rotate("+degrees+"deg)";
-  }, 1000);
-  stopButton.disabled = false;
-  resetButton.disabled = false;
-  startButton.disabled = true;
+function countdownTimerBegin() {
+	if (!canvas.classList.contains("animate")) {
+		canvas.classList.add("animate");
+		pomodoroDiv.classList.add("animate", "reverse");
+		stateToEstablish = true;
+		beginRotations(stateToEstablish);
+	} else {
+		stateToEstablish = true;
+		beginRotations(stateToEstablish);
+	}
+	stopButton.disabled = false;
+	resetButton.disabled = false;
+	startButton.disabled = true;
+
+	timer = setInterval(function() {
+		timerMiliSeconds--;
+		if (timerMiliSeconds % 250 == 0) {
+			if (timerMinutes > 0 && timerSeconds == 0) {
+				timerMinutes -= 1;
+				timerSeconds = 59;
+			} else if (timerMinutes == 0 && timerSeconds == 0) {
+				stateToEstablish = false;
+				alert("Time's up!");
+				beginRotations(stateToEstablish);
+				timerMinutes = 30;
+				timerSeconds = 0;
+			} else {
+				timerSeconds--;
+			}
+			document.getElementById("timer-minutes").innerHTML = timerMinutes;
+			document.getElementById("timer-seconds").innerHTML = timerSeconds;
+		}
+	}, 1);
 }
 
-function stopAndStart(){
-  if(timer){
-    clearInterval(timer);
-    startButton.innerHTML = "Click Here To Continue";
-    stopButton.disabled = true;
-    startButton.disabled = false;
-    resetButton.disabled = false;
-  }
+function stopAndStart() {
+	if (timer) {
+		stateToEstablish = false;
+		beginRotations(stateToEstablish);
+		clearInterval(timer);
+		startButton.innerHTML = "Click Here To Continue";
+		stopButton.disabled = true;
+		startButton.disabled = false;
+		resetButton.disabled = false;
+	}
 }
 
+function stopAndReset() {
+	stateToEstablish = false;
+	beginRotations(stateToEstablish);
+	clearInterval(timer);
+	timerMinutes = 30;
+	timerSeconds = 0;
+	timerMiliSeconds = timerMinutes * 60000 + timerSeconds * 1000;
+	document.getElementById("timer-minutes").innerHTML = timerMinutes;
+	document.getElementById("timer-seconds").innerHTML = timerSeconds;
+	startButton.innerHTML = "Click Here To Begin!";
+	resetButton.disabled = true;
+	stopButton.disabled = true;
+	startButton.disabled = false;
 
-function stopAndReset(){
-  clearInterval(timer);
-  timerMinutes = 30;
-  timerSeconds = 0;
-  document.getElementById('timer-minutes').innerHTML = timerMinutes;
-  document.getElementById('timer-seconds').innerHTML = timerSeconds;
-  startButton.innerHTML = "Click Here To Begin!";
-  resetButton.disabled = true;
-  stopButton.disabled = true;
-  startButton.disabled = false;
+	setTimeout("canvas.classList.remove('animate');", 10);
+	setTimeout("pomodoroDiv.classList.remove('animate','reverse');", 10);
 }
 
-function beginRotations(){
-  canvasDegrees += 360;
-  pomodoroDegrees -= 360;
-  pomodoroDiv.style.transform = "rotate("+pomodoroDegrees+"deg)";
-  canvas.style.transform = "rotate("+canvasDegrees+"deg)";
+function beginRotations(state) {
+	if (!state) {
+		canvas.style.animationPlayState = "paused";
+		pomodoroDiv.style.animationPlayState = "paused";
+	} else {
+		canvas.style.animationPlayState = "running";
+		pomodoroDiv.style.animationPlayState = "running";
+	}
 }
